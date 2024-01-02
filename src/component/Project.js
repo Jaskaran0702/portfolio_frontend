55ae01ced434bae2c56c5c929b39307428e2deb5
Jaskaran0702 <21ucs097@lnmiit.ac.in>
2024-01-02 23:01:39 +0530

    projects updated

--- src/component/Project.js
+++ src/component/Project.js
@@ -5,24 +5,39 @@ import cb from "../assets/images/crazy_bus.png";
 import vt from "../assets/images/vital_track.png";
 import ids from "../assets/images/ids.png";
 import dl from "../assets/images/dl.png";
+import rl from '../assets/images/real_estate.png'
+import blog from '../assets/images/blog.png'
+import file from '../assets/images/file-sh.png'
+import studbud from '../assets/images/studbud.png'
+import tabman from '../assets/images/tab-man.png'
 export const Project = () => {
     const projects1 = [
         {
-            title:"Crazy Bus",
-            description: "Crazy Bus is a full-stack project, redefines the LNMIIT travel experience. Tailored for students, it streamlines bus selection, seat reservation, and secure payments. It has user-centric design, emphasizing how Crazy Bus enhances convenience for LNMIIT students. From the intuitive interface to robust functionality, the project showcases a seamless and reliable bus booking platform, addressing the unique needs of the LNMIIT community.",
-            imgUrl: cb
+            title:"JB Estates",
+            description: "JB Estates revolutionizes real estate with its comprehensive platform, streamlining property browsing, effortless listing creation, and facilitating direct owner communication through provided contact details. This user-centric approach ensures transparency and efficiency in transactions, setting a new standard for convenience and accessibility in the dynamic property market.",
+            imgUrl: rl
         },
         {
-            title:"Algorithm Visualiser",
-            description: "The Algorithm Visualizer project is currently in progress, with the goal of providing an interactive visualization of common sorting algorithms. This tool will allow users to observe the step-by-step execution of sorting algorithms with adjustable input sizes and variable numbers. By enhancing understanding through visual representation, the project aims to make learning algorithms engaging and accessible, fostering a deeper comprehension of sorting processes.",
-            imgUrl: av
+            title:"SocialHub",
+            description: "SocialHub, an innovative full-stack project, revolutionizes social media engagement by offering a user-centric platform for seamless post creation, sharing, and diverse reactions, all within a secure and intuitive environment that prioritizes authentic connections in the digital realm.",
+            imgUrl: blog
+        },
+        {
+            title:"FileFlow",
+            description: "FileFlow, a cutting-edge full-stack project, transforms file sharing by enabling users to effortlessly upload files, convert them into secure downloadable links, and share with unparalleled ease, simplifying the process of distributing large files.",
+            imgUrl: file
         }
     ];
     const projects2 = [
         {
-            title:"Vital-Track",
-            description: "This project is a prototype for monitoring different body vital and environmental conditions. The system uses a variety of sensors, including a temperature sensor, an environmental sensor, and a network connection. The data collected from these sensors is then displayed on a web dashboard and uploaded to a cloud platform for further data analysis and real-time monitoring.",
-            imgUrl: vt
+            title:"StudyBuddy",
+            description: "StudyBuddy, a Chrome extension, optimizes focus by blocking distracting sites during study sessions. The intuitive study mode is easily toggled on, preventing interruptions. For urgent needs, a snooze button with a timer offers flexibility without compromising productivity, ensuring an efficient and distraction-free learning experience.",
+            imgUrl: studbud
+        },
+        {
+            title:"Tab Manager",
+            description: "Introducing Tab Manager, a Chrome extension designed for seamless tab organization. This efficient tool simplifies tab management, allowing users to effortlessly arrange, group, and navigate through open tabs. With a streamlined interface, Tab Manager enhances productivity, ensuring a clutter-free browsing experience. Simplify your Chrome experience with Tab Manager, the go-to solution for efficient tab organization.",
+            imgUrl: tabman
         }
     ];
     const projects3 = [
@@ -48,10 +63,10 @@ export const Project = () => {
                     <Tab.Container id="projetcs-tabs" defaultActiveKey="first">
                     <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                         <Nav.Item>
-                            <Nav.Link eventKey="first">Web Develpement</Nav.Link>
+                            <Nav.Link eventKey="first">Web Developement</Nav.Link>
                         </Nav.Item>
                         <Nav.Item>
-                            <Nav.Link eventKey="second">Other</Nav.Link>
+                            <Nav.Link eventKey="second">Chrome Extensions</Nav.Link>
                         </Nav.Item>
                         <Nav.Item>
                             <Nav.Link eventKey="third">ML/DL</Nav.Link>
